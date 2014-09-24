325c13757786ae58be110d44f09d3db072af43c1
Jess <jess@Embers-Computer-8.local>
2014-09-23 19:46:16 -0600

    added arms subsystem and outlined functions

---
+++ src/org/team1619/subsystems/Arms.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.team1619.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ * @author jess
+ */
+public class Arms extends Subsystem {
+
+    protected void initDefaultCommand() {
+
+    }
+    
+    protected void collect() {
+        
+    }
+    
+    protected void pullBack() {
+        
+    }
+    
+}

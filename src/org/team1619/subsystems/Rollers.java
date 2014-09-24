ad6c4d47c753ef776f8fff2081d7cdf9f245d3be
Jess <jess@Embers-Computer-8.local>
2014-09-23 19:32:40 -0600

    added rollers subsystem and outlined functions

---
+++ src/org/team1619/subsystems/Rollers.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.team1619.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ * @author jess
+ */
+public class Rollers extends Subsystem {
+    
+    protected void initDefaultCommand() {
+        
+    }
+    
+    protected void rollersForward() {
+        
+    }
+    
+    protected void rollersReverse() {
+        
+    }
+    
+}

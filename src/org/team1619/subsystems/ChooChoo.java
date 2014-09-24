38c9ffb39b702ab20e294cf00f4c5258d0e75758
Jess <jess@Embers-Computer-8.local>
2014-09-23 19:43:17 -0600

    added choochoo subsystem and outlined functions

---
+++ src/org/team1619/subsystems/ChooChoo.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.team1619.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ * @author jess
+ */
+public class ChooChoo extends Subsystem {
+
+    protected void initDefaultCommand() {
+      
+    }
+    
+    protected void run() {
+        
+    }
+    
+}

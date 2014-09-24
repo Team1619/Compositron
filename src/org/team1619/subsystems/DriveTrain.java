42db4141c7b950025542f61409352fe0550cf9f6
Jess <jess@Embers-Computer-8.local>
2014-09-23 19:52:25 -0600

    added drivetrain subsystem

---
+++ src/org/team1619/subsystems/DriveTrain.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.team1619.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ * @author jess
+ */
+public class DriveTrain extends Subsystem {
+
+    protected void initDefaultCommand() {
+
+    }
+
+}

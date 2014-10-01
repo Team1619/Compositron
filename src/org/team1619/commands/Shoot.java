f167c381288779882fce1bc9e1233839e526f9a2
Jess <jess@Embers-Computer-8.local>
2014-09-30 18:50:26 -0600

    added Shoot command

---
+++ src/org/team1619/commands/Shoot.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.team1619.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ * @author jess
+ */
+public class Shoot extends Command {
+
+    protected void initialize() {
+
+    }
+
+    protected void execute() {
+
+    }
+
+    protected boolean isFinished() {
+        return true;
+    }
+
+    protected void end() {
+
+    }
+
+    protected void interrupted() {
+
+    }
+    
+}
+
